bbe12a116c3e8bd7d688f0b575c8e411f86c9ada
Clemens Prescher <clemens.prescher@gmail.com>
2015-01-31 08:31:15 -0600

    finished basic NewRoiView

--- T-Rax/test/test_NewRoiView.py
+++ T-Rax/test/test_NewRoiView.py
@@ -3,11 +3,84 @@ __author__ = 'Clemens Prescher'
 
 
 import unittest
+import numpy as np
+from PyQt4 import QtGui
+
+from view.NewRoiView import NewRoiView
+
 
 
 class TestNewRoiView(unittest.TestCase):
     def setUp(self):
-        pass
+        self.app = QtGui.QApplication([])
+        self.view = NewRoiView()
+        self.img_roi = self.view.img_widget.rois[0]
+        self.roi_gb = self.view.roi_gbs[0]
 
     def tearDown(self):
-        pass
+        del self.app
+
+
+    def test_changing_the_img_roi_changes_the_text_fields(self):
+        self.img_roi.setPos(100,100)
+
+        x_min_gb = np.round(float(str(self.roi_gb.x_min_txt.text())))
+        y_min_gb = np.round(float(str(self.roi_gb.y_min_txt.text())))
+
+        self.assertEqual(x_min_gb, 100)
+        self.assertEqual(y_min_gb, 100)
+
+        self.img_roi.setSize(100,100)
+
+        x_max_gb = np.round(float(str(self.roi_gb.x_max_txt.text())))
+        y_max_gb = np.round(float(str(self.roi_gb.y_max_txt.text())))
+
+        self.assertEqual(x_max_gb, 200)
+        self.assertEqual(y_max_gb, 200)
+
+    def test_changing_text_field_changes_img_rois(self):
+        self.roi_gb.x_min_txt.setText('10')
+        self.roi_gb.y_min_txt.setText('13')
+
+        self.roi_gb.roi_txt_changed.emit([10, 200, 13, 200])
+
+        rounded_roi_pos = np.round(self.img_roi.pos())
+        self.assertEqual(rounded_roi_pos[0], 10)
+        self.assertEqual(rounded_roi_pos[1], 13)
+
+
+    def test_set_rois(self):
+        # the set rois function should not emit any signal to avoid circular feedback loops:
+
+        self.counter = 0
+        def incr_counter():
+            self.counter += 1
+        self.view.rois_changed.connect(incr_counter)
+        self.view.set_rois([[10,100,11,101]])
+
+        self.assertEqual(self.counter, 0)
+
+        x_min_gb = np.round(float(str(self.roi_gb.x_min_txt.text())))
+        x_max_gb = np.round(float(str(self.roi_gb.x_max_txt.text())))
+        y_min_gb = np.round(float(str(self.roi_gb.y_min_txt.text())))
+        y_max_gb = np.round(float(str(self.roi_gb.y_max_txt.text())))
+
+        self.assertEqual(x_min_gb, 10)
+        self.assertEqual(x_max_gb, 100)
+        self.assertEqual(y_min_gb, 11)
+        self.assertEqual(y_max_gb, 101)
+
+        rounded_roi_pos = np.round(self.img_roi.pos())
+        rounded_roi_size = np.round(self.img_roi.size())
+
+        x_min_img_roi = rounded_roi_pos[0]
+        x_max_img_roi = rounded_roi_pos[0] + rounded_roi_size[0]
+        y_min_img_roi = rounded_roi_pos[1]
+        y_max_img_roi = rounded_roi_pos[1] + rounded_roi_size[1]
+
+        self.assertEqual(x_min_img_roi, 10)
+        self.assertEqual(x_max_img_roi, 100)
+        self.assertEqual(y_min_img_roi, 11)
+        self.assertEqual(y_max_img_roi, 101)
+
+
--- T-Rax/view/NewRoiView.py
+++ T-Rax/view/NewRoiView.py
@@ -3,46 +3,82 @@ __author__ = 'Clemens Prescher'
 
 from PyQt4 import QtCore, QtGui
 import pyqtgraph as pg
+from functools import partial
+import numpy as np
+from pyqtgraph.graphicsItems.ROI import Handle
 
 
 class NewRoiView(QtGui.QWidget):
-    def __init__(self, *args, **kwargs):
+    rois_changed = QtCore.pyqtSignal(list)
+
+    def __init__(self, roi_num=1, roi_titles = ('',), roi_colors=((255, 255, 0)), *args, **kwargs):
         super(NewRoiView, self).__init__(*args, **kwargs)
+        self.roi_num = roi_num
+        self.roi_titles = roi_titles
+        self.roi_colors = roi_colors
 
         self._main_horizontal_layout = QtGui.QHBoxLayout()
-        self._limits_layout = QtGui.QVBoxLayout()
 
-        self._main_horizontal_layout.addWidget(QtGui.QWidget())# placeholder for image view
 
-        self.img_widget = ImageWidget()
-
-        self.us_roi_gb = RoiGroupBox('Upstream ROI')
-        self.ds_roi_gb = RoiGroupBox('Downstream ROI')
-        self._limits_layout.addWidget(self.us_roi_gb)
-        self._limits_layout.addWidget(self.ds_roi_gb)
-        self._limits_layout.addSpacerItem(QtGui.QSpacerItem(20,20,
+        self._roi_gbs_layout = QtGui.QVBoxLayout()
+        self.roi_gbs = []
+        self.create_roi_gbs()
+        self._roi_gbs_layout.addSpacerItem(QtGui.QSpacerItem(20, 20,
                                                             QtGui.QSizePolicy.Expanding,
                                                             QtGui.QSizePolicy.Expanding))
 
-        self._main_horizontal_layout.addWidget(self.img_widget)
-        self._main_horizontal_layout.addLayout(self._limits_layout)
 
+        self.img_widget = ImageWidget(roi_num=roi_num, roi_colors=roi_colors)
+        self._main_horizontal_layout.addWidget(self.img_widget)
+
+        self._main_horizontal_layout.addLayout(self._roi_gbs_layout)
         self.setLayout(self._main_horizontal_layout)
+
+        self.create_signals()
+
+    def create_roi_gbs(self):
+        for ind in range(self.roi_num):
+            self.roi_gbs.append(RoiGroupBox(self.roi_titles[ind], self.roi_colors[ind]))
+            self.roi_gbs[-1].roi_txt_changed.connect(partial(self.update_img_roi, ind))
+            self._roi_gbs_layout.addWidget(self.roi_gbs[-1])
+
+    def create_signals(self):
+        self.img_widget.rois_changed.connect(self.update_roi_gbs)
+
+    def update_roi_gbs(self, rois_list):
+        for ind, roi_gb in enumerate(self.roi_gbs):
+            roi_gb.blockSignals(True)
+            roi_gb.update_roi_txt(rois_list[ind])
+            roi_gb.blockSignals(False)
+        self.rois_changed.emit(self.img_widget.get_roi_limits())
+
+    def update_img_roi(self, ind, roi_list):
+        self.img_widget.blockSignals(True)
+        self.img_widget.update_roi(ind, roi_list)
+        self.img_widget.blockSignals(False)
+        self.rois_changed.emit(self.img_widget.get_roi_limits())
+
+    def set_rois(self, rois_list):
+        self.blockSignals(True)
+        self.update_roi_gbs(rois_list)
+        for ind in range(self.roi_num):
+            self.update_img_roi(ind, rois_list[ind])
+        self.blockSignals(False)
 
 
 
 class RoiGroupBox(QtGui.QGroupBox):
-    def __init__(self, title):
+    roi_txt_changed = QtCore.pyqtSignal(list)
+
+    def __init__(self, title, color):
         super(RoiGroupBox, self).__init__(title)
+        self.color = color
         self._grid_layout = QtGui.QGridLayout()
 
         self.x_min_txt = IntegerTextField('0')
         self.x_max_txt = IntegerTextField('0')
         self.y_min_txt = IntegerTextField('0')
         self.y_max_txt = IntegerTextField('0')
-
-        self._grid_layout.addWidget(CenteredQLabel('min'), 0, 1)
-        self._grid_layout.addWidget(CenteredQLabel('max'), 0, 2)
 
         self._grid_layout.addWidget(CenteredQLabel('X:'), 1, 0)
         self._grid_layout.addWidget(self.x_min_txt, 1, 1)
@@ -53,51 +89,115 @@ class RoiGroupBox(QtGui.QGroupBox):
         self._grid_layout.addWidget(self.y_max_txt, 2, 2)
 
         self.setLayout(self._grid_layout)
+        style_str = "color: {0}; border: 1px solid {0};".format(self.color)
+        self.setStyleSheet('QGroupBox {'+style_str+'}')
+
+        self.create_signals()
+
+    def create_signals(self):
+        self.x_min_txt.editingFinished.connect(self._roi_txt_changed)
+        self.x_max_txt.editingFinished.connect(self._roi_txt_changed)
+        self.y_min_txt.editingFinished.connect(self._roi_txt_changed)
+        self.y_max_txt.editingFinished.connect(self._roi_txt_changed)
+
+    def get_roi_limits(self):
+        x_min = int(str(self.x_min_txt.text()))
+        x_max = int(str(self.x_max_txt.text()))
+        y_min = int(str(self.y_min_txt.text()))
+        y_max = int(str(self.y_max_txt.text()))
+
+        return [x_min, x_max, y_min, y_max]
+
+    def update_roi_txt(self, roi_list):
+        self.x_min_txt.setText(str(int(np.round(roi_list[0]))))
+        self.x_max_txt.setText(str(int(np.round(roi_list[1]))))
+        self.y_min_txt.setText(str(int(np.round(roi_list[2]))))
+        self.y_max_txt.setText(str(int(np.round(roi_list[3]))))
+
+    def _roi_txt_changed(self):
+        self.roi_txt_changed.emit(self.get_roi_limits())
+
 
 class CenteredQLabel(QtGui.QLabel):
     def __init__(self, *args, **kwargs):
         super(CenteredQLabel, self).__init__(*args, **kwargs)
         self.setAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignHCenter)
 
+
 class IntegerTextField(QtGui.QLineEdit):
     def __init__(self, *args, **kwargs):
         super(IntegerTextField, self).__init__(*args, **kwargs)
         self.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignVCenter)
         self.setValidator(QtGui.QIntValidator())
-        
-        
+
+
+
+
+
 class ImageWidget(QtGui.QWidget):
     mouse_moved = QtCore.pyqtSignal(float, float)
     mouse_left_clicked = QtCore.pyqtSignal(float, float)
     mouse_left_double_clicked = QtCore.pyqtSignal(float, float)
 
-    def __init__(self, *args, **kwargs):
+    rois_changed = QtCore.pyqtSignal(list)
+
+    def __init__(self, roi_num=1, roi_colors=((255, 255, 0)), *args, **kwargs):
         super(ImageWidget, self).__init__(*args, **kwargs)
+        self.roi_num = roi_num
+        self.roi_colors = roi_colors
+
         self.pg_widget = pg.GraphicsLayoutWidget()
         self.pg_layout = self.pg_widget.ci
-        self.pg_layout.setContentsMargins(0,10,15,0)
-        self.pg_viewbox = self.pg_layout.addViewBox(1, 1, lockAspect = False)
+        self.pg_layout.setContentsMargins(0, 10, 15, 0)
+        self.pg_viewbox = self.pg_layout.addViewBox(1, 1, lockAspect=False)
 
         self.bottom_axis = pg.AxisItem('bottom', linkView=self.pg_viewbox)
         self.left_axis = pg.AxisItem('left', linkView=self.pg_viewbox)
 
         self.pg_layout.addItem(self.bottom_axis, 2, 1)
-        self.pg_layout.addItem(self.left_axis, 1,0)
+        self.pg_layout.addItem(self.left_axis, 1, 0)
 
         self.pg_img_item = pg.ImageItem()
         self.pg_viewbox.addItem(self.pg_img_item)
 
         self._layout = QtGui.QHBoxLayout()
-        self._layout.setContentsMargins(0,0,0,0)
+        self._layout.setContentsMargins(0, 0, 0, 0)
         self._layout.addWidget(self.pg_widget)
         self.setLayout(self._layout)
 
+        self.add_rois()
+
         self.modify_mouse_behavior()
+
+    def add_rois(self):
+        self.rois = []
+        for ind in range(self.roi_num):
+            self.rois.append(ImgROI((25, 25), (150, 150),
+                                    pen=pg.mkPen(self.roi_colors[ind], width=4),
+                                    active_pen=pg.mkPen('r', width=4)))
+            self.pg_viewbox.addItem(self.rois[-1])
+            self.rois[-1].sigRegionChanged.connect(self.roi_changed)
+
+    def get_roi_limits(self):
+        roi_limits = []
+        for roi in self.rois:
+            roi_limits.append([roi.pos()[0], roi.pos()[0]+roi.size()[0],
+                               roi.pos()[1], roi.pos()[1]+roi.size()[1]])
+        return roi_limits
+
+    def roi_changed(self):
+        self.rois_changed.emit(self.get_roi_limits())
+
+    def update_roi(self, ind, roi_limits):
+        self.rois[ind].setPos((roi_limits[0], roi_limits[2]))
+        self.rois[ind].setSize((roi_limits[1]-roi_limits[0],
+                                roi_limits[3]- roi_limits[2]))
+
 
     def plot_image(self, data):
         self.pg_img_item.setImage(data)
         x_max, y_max = data.shape
-        self.pg_viewbox.setLimits(xMin=0, xMax =x_max,
+        self.pg_viewbox.setLimits(xMin=0, xMax=x_max,
                                   yMin=0, yMax=y_max)
         self.img_data = data
 
@@ -106,7 +206,7 @@ class ImageWidget(QtGui.QWidget):
         self.mouse_moved.emit(pos.x(), pos.y())
 
     def modify_mouse_behavior(self):
-        #different mouse handlers
+        # different mouse handlers
         self.pg_viewbox.setMouseMode(self.pg_viewbox.RectMode)
 
         self.pg_layout.scene().sigMouseMoved.connect(self.mouseMoved)
@@ -134,7 +234,7 @@ class ImageWidget(QtGui.QWidget):
             self.mouse_left_double_clicked.emit(pos.x(), pos.y())
 
     def myMouseDragEvent(self, ev, axis=None):
-        #most of this code is copied behavior of left click mouse drag from the original code
+        # most of this code is copied behavior of left click mouse drag from the original code
         ev.accept()
         pos = ev.pos()
         lastPos = ev.lastPos()
@@ -172,10 +272,46 @@ class ImageWidget(QtGui.QWidget):
                 self.pg_viewbox.updateScaleBox(ev.buttonDownPos(), ev.pos())
 
 
+class ImgROI(pg.ROI):
+    def __init__(self, pos, size, pen, active_pen):
+        super(ImgROI, self).__init__(pos, size, False, True)
+
+        self.setPen(pen)
+        self.active_pen = active_pen
+
+        self.addScaleHandle([1, 0.5], [0, 0.5], item=Handle(10, typ='f', pen=pen, activePen=active_pen, parent=self))
+        self.addScaleHandle([0.5, 1], [0.5, 0], item=Handle(10, typ='f', pen=pen, activePen=active_pen, parent=self))
+        self.addScaleHandle([0, 0.5], [1, 0.5], item=Handle(10, typ='f', pen=pen, activePen=active_pen, parent=self))
+        self.addScaleHandle([0.5, 0], [0.5, 1], item=Handle(10, typ='f', pen=pen, activePen=active_pen, parent=self))
+
+        self.addScaleHandle([1, 0], [0, 1], item=Handle(8, typ='s', pen=pen, activePen=active_pen, parent=self))
+        self.addScaleHandle([0, 1], [1, 0], item=Handle(8, typ='s', pen=pen, activePen=active_pen, parent=self))
+        self.addScaleHandle([0, 0], [1, 1], item=Handle(8, typ='s', pen=pen, activePen=active_pen, parent=self))
+        self.addScaleHandle([1, 1], [0, 0], item=Handle(8, typ='s', pen=pen, activePen=active_pen, parent=self))
+
+
+    def hoverEvent(self, ev):
+        hover = False
+        if not ev.isExit():
+            if ev.acceptDrags(QtCore.Qt.LeftButton):
+                hover = True
+            for btn in [QtCore.Qt.LeftButton, QtCore.Qt.RightButton, QtCore.Qt.MidButton]:
+                if int(self.acceptedMouseButtons() & btn) > 0 and ev.acceptClicks(btn):
+                    hover = True
+
+        if hover:
+            self.currentPen = self.active_pen
+        else:
+            self.currentPen = self.pen
+        self.update()
+
+
 if __name__ == '__main__':
     import numpy as np
+
     app = QtGui.QApplication([])
-    view = NewRoiView()
+    view = NewRoiView(2, ['Downstream ROI', 'Upstream_ROI'],
+        ('#FFFF00','#FF9900'))
 
     data = np.identity(200)
     view.img_widget.plot_image(data)
