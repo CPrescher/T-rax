6aebe6cd72662fe1a7b35fbb5157e92a8e49eda1
Clemens Prescher <clemens.prescher@gmail.com>
2015-05-22 16:10:06 -0500

    finished ramanmodel

--- t-rax/model/RamanModel.py
+++ t-rax/model/RamanModel.py
@@ -1,9 +1,36 @@
 # -*- coding: utf8 -*-
 __author__ = 'Clemens Prescher'
 
+import numpy as np
+
 from .BaseModel import SingleSpectrumModel
+
+REVERSE_CM_MODE = 0
+WAVELENGTH_MODE = 1
 
 
 class RamanModel(SingleSpectrumModel, object):
     def __init__(self):
         super(RamanModel, self).__init__()
+        self._laser_line = 532
+        self.mode = REVERSE_CM_MODE
+
+    @property
+    def spectrum(self):
+        spec = super(RamanModel, self).spectrum
+        if self.mode is REVERSE_CM_MODE:
+            spec._x = convert_wavelength_to_reverse_cm(spec.x, self.laser_line)
+        return spec
+
+    @property
+    def laser_line(self):
+        return self._laser_line
+
+    @laser_line.setter
+    def laser_line(self, value):
+        self._laser_line = value
+        self.spectrum_changed.emit(*self.spectrum.data)
+
+
+def convert_wavelength_to_reverse_cm(wavelength, laser_line):
+    return (1.0 / laser_line - 1 / np.array(wavelength)) * 1.0e7
---
+++ t-rax/test/test_RamanController.py
@@ -0,0 +1,38 @@
+# -*- coding: utf8 -*-
+__author__ = 'Clemens Prescher'
+
+import unittest
+import os
+import numpy as np
+
+from model.RamanModel import RamanModel, WAVELENGTH_MODE
+
+unittest_path = os.path.dirname(__file__)
+unittest_files_path = os.path.join(unittest_path, 'test_files')
+test_file = os.path.join(unittest_files_path, 'temper_009.spe')
+
+
+class RamanModelTest(unittest.TestCase):
+    def setUp(self):
+        self.model = RamanModel()
+
+    def test_changing_laser_line(self):
+        self.model.load_file(test_file)
+
+        x, y = self.model.spectrum.data
+
+        self.model.laser_line = 600
+
+        x_new, y_new = self.model.spectrum.data
+
+        self.assertFalse(np.array_equal(x, x_new))
+        self.assertTrue(np.array_equal(y, y_new))
+
+    def test_changing_mode(self):
+        self.model.load_file(test_file)
+
+        x, y = self.model.spectrum.data
+        self.model.mode = WAVELENGTH_MODE
+        x_new, y_new = self.model.spectrum.data
+        self.assertFalse(np.array_equal(x, x_new))
+        self.assertTrue(np.array_equal(y, y_new))
