9f91e64b0790bdec4c1a4be3963bd99971864eb6
Eran Greenberg <erangre@gmail.com>
2017-11-02 15:44:58 -0500

    Enabled saving of data when US and DS are different size regions (using separate files) Modified the test of the temperature model accordingly fixed two problems with print in the test file.

--- t-rax/test/model_test/test_TemperatureModel.py
+++ t-rax/test/model_test/test_TemperatureModel.py
@@ -478,8 +478,8 @@ class TestTemperatureModel(unittest.TestCase):
 
         self.model2.load_setting(setting_filename)
 
-        print self.model.ds_roi.as_list()
-        print self.model2.ds_roi.as_list()
+        print(self.model.ds_roi.as_list())
+        print(self.model2.ds_roi.as_list())
         self.assertEqual(self.model.ds_temperature, self.model2.ds_temperature)
         self.assertEqual(self.model.us_temperature, self.model2.us_temperature)
 
@@ -518,7 +518,10 @@ class TestTemperatureModel(unittest.TestCase):
         out_path = os.path.join(unittest_files_path, 'data.txt')
         self.model.save_txt(out_path)
 
-        file = open(out_path)
+        ds_out_path = str(out_path).rsplit('.', 1)[0] + '_ds.txt'
+        us_out_path = str(out_path).rsplit('.', 1)[0] + '_us.txt'
+
+        file = open(ds_out_path)
         lines = file.readlines()
 
         self.assertEqual(lines[0], "# Fitted Temperatures:\n")
@@ -526,7 +529,20 @@ class TestTemperatureModel(unittest.TestCase):
         self.assertEqual(lines[2], "# Upstream (K): 1413.6	2.1\n")
         self.assertEqual(lines[3], "# \n")
         self.assertEqual(lines[4], "# Datacolumns:\n")
-        self.assertEqual(lines[5], "# lambda(nm)	DS_data	DS_fit	US_data	US_fit\n")
+        self.assertEqual(lines[5], "# lambda(nm)	DS_data	DS_fit\n")
 
         file.close()
-        os.remove(out_path)
+        os.remove(ds_out_path)
+
+        file = open(us_out_path)
+        lines = file.readlines()
+
+        self.assertEqual(lines[0], "# Fitted Temperatures:\n")
+        self.assertEqual(lines[1], "# Downstream (K): 1046.9	14.7\n")
+        self.assertEqual(lines[2], "# Upstream (K): 1413.6	2.1\n")
+        self.assertEqual(lines[3], "# \n")
+        self.assertEqual(lines[4], "# Datacolumns:\n")
+        self.assertEqual(lines[5], "# lambda(nm)	US_data	US_fit\n")
+
+        file.close()
+        os.remove(us_out_path)
