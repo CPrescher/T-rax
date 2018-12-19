dcf6d3fb19db6bb9ee3c3ac3f2337634fb86a1cf
Matt Newville <newville@cars.uchicago.edu>
2018-12-19 11:10:52 -0600

    Create _init_.py, with run_t_rax() function

---
+++ t_rax/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# T-Rax - GUI program for analysis of spectroscopy data during
+# diamond anvil cell experiments
+# Copyright (C) 2016 Clemens Prescher (clemens.prescher@gmail.com)
+# Institute for Geology and Mineralogy, University of Cologne
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+from sys import platform
+
+from qtpy import QtWidgets
+
+from .controller.MainController import MainController, get_version
+
+def run_t_rax():
+    app = QtWidgets.QApplication(sys.argv)
+    if platform != "darwin":
+        app.setStyle('plastique')
+    controller = MainController()
+    controller.show_window()
+    app.exec_()
+
