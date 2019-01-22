3c6c4d9dd57097f0e391c9bf49362288fccfaa6c
Eran Greenberg <erangre@gmail.com>
2019-01-22 15:23:37 -0600

    Changed the file that runs t_rax so it works with a simple import of the t_rax folder changed to build 6

---
+++ run_t_rax.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# T-Rax - GUI program for analysis of spectroscopy data during
+# diamond anvil cell experiments
+# Copyright (C) 2016 Clemens Prescher (clemens.prescher@gmail.com)
+# Institute for Geology and Mineralogy, University of Cologne
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# from __future__ import absolute_import
+
+import t_rax
+t_rax.run_t_rax()
